0d33def991f1dab5f2b3d2f4bd24154f52698e5f
Andre Faure <andre.faure@camelotglobal.com>
2018-02-21 00:04:39 +0000

    add more ram to master add temp work-around dashboard-admin.yaml to bypass auth on dashboard (somewhat ok on this private vagrant cluster until I can look at setting up tokens) add calico networking (using default params which ad good-enough for this. 2-step install so I can customise the config later on) add heapster

--- Vagrantfile
+++ Vagrantfile
@@ -14,7 +14,7 @@ Vagrant.configure("2") do |config|
     master.vm.network "private_network", ip: "#{master_ip}"
     master.vm.provision "shell", path: "master.sh"
     master.vm.provider :virtualbox do |vb|
-      vb.customize [ "modifyvm", :id, "--memory", "768" ]
+      vb.customize [ "modifyvm", :id, "--memory", "1024" ]
     end
   end
 
--- master.sh
+++ master.sh
@@ -3,7 +3,7 @@
 
 /vagrant/ubuntu.sh
 
-kubeadm init --apiserver-advertise-address 172.16.0.10 | tee /vagrant/kubeadmin.out
+kubeadm init --apiserver-advertise-address 172.16.0.10 --pod-network-cidr=192.168.0.0/16 | tee /vagrant/kubeadmin.out
 grep "kubeadm join" /vagrant/kubeadmin.out > /vagrant/join.sh
 chmod +x /vagrant/join.sh
 cp /etc/kubernetes/admin.conf /vagrant/
@@ -26,16 +26,13 @@ do
   sleep 5
 done
 
-# install weave
-kubectl apply -f https://git.io/weave-kube-1.6
-echo "Wating for weave to start"
-while true
-do
-  kubectl -n kube-system get pods -l name=weave-net | grep -q Running
-  [ $? -eq 0 ] && break
-  echo -n "."
-  sleep 5
-done
+# install calico
+curl -L https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml \
+ -o /tmp/calico.yaml
+kubectl apply -f /tmp/calico.yaml
+
+# install heapster
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/standalone/heapster-controller.yaml
 
 # install ddashboard
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml
